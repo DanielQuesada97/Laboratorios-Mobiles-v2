57e3d0da0f0554615bfc9b4bd2ca956430a38b7a
Daniel Quesada <danielquesada@Daniels-MacBook-Pro.local>
2023-05-02 14:43:36 -0600

    Laboratorio 07 commit

---
+++ Laboratorio07 DataBinding/app/src/main/java/com/quesada/laboratorio05_navigationcomponent/data/model/MovieModel.kt
@@ -0,0 +1,9 @@
+package com.quesada.laboratorio05_navigationcomponent.data.model
+
+data class MovieModel (
+    val name: String,
+    val category: String,
+    val description: String,
+    val qualification: String
+)
+
---
+++ Laboratorio07 DataBinding/app/src/main/java/com/quesada/laboratorio05_navigationcomponent/ui/movie/Movie.kt
@@ -0,0 +1,55 @@
+package com.quesada.laboratorio05_navigationcomponent.ui.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.quesada.laboratorio05_navigationcomponent.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Movie.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Movie : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Movie.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Movie().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
