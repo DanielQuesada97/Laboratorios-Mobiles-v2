9e0229c552182bb01ff533992d4267c078cbaeb1
Daniel Quesada <danielquesada@Daniels-MacBook-Pro.local>
2023-05-30 14:22:46 -0600

    Labo 10 commit

--- Laboratorio08 RecyclerView/app/src/main/java/com/quesada/laboratorio05_navigationcomponent/ui/MainActivity.kt
+++ Laboratorio08 RecyclerView/app/src/main/java/com/quesada/laboratorio05_navigationcomponent/ui/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.quesada.laboratorio05_navigationcomponent.R
 
-
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
